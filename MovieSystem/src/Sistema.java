24fbedd8d0331c2a754e99abdc12e335c3bb5367
RJA96 <rodrigoaguilar96@gmail.com>
2018-10-04 13:35:31 -0300

    Modificaciones Sistema

--- MovieSystem/src/Sistema.java
+++ MovieSystem/src/Sistema.java
@@ -9,17 +9,36 @@ public class Sistema {
 	public void Agregarusuario(Usuario u) {
 		this.usuarios.add(u);
 	}
+	
 	public void Agregarpelicula(Pelicula p) {
 		this.peliculas.add(p);
 	}
+	
 	public void Agregargrupo(Grupo g) {
 		this.grupos.add(g);
 	}
+	
 	public void VerPelicula(Usuario u,Pelicula p, int clasif) {
 		u.VerPelicula(p);
 		u.ClasificarPeli(p, clasif);
-		
-		
+	}
+	
+	public ArrayList<Pelicula> RecomendarPeli(Usuario u){
+		ArrayList<Pelicula> aux = new ArrayList<>();
+		return aux;
+	}
+	
+	public int CantidadVotos(Pelicula p) {
+		return p.getCantdevotos();
+	}
+	
+	public double PromedioClasf(Pelicula p) {
+		return p.getClasificacion();
+	}
+	
+	public ArrayList<Pelicula>BuscarPelicula(String c){
+		ArrayList<Pelicula> aux = new ArrayList<>();
+		return aux;
 	}
 
 }
