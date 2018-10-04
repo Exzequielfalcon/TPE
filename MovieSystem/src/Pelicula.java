4d6a3762c9d09ab00bdc1df2fc5e6a477a922f4e
RJA96 <rodrigoaguilar96@gmail.com>
2018-10-04 13:12:11 -0300

    casi terminado peliculas

--- MovieSystem/src/Pelicula.java
+++ MovieSystem/src/Pelicula.java
@@ -1,4 +1,63 @@
+import java.util.ArrayList;
 
 public class Pelicula {
+	String titulo;
+	String sinopsis;
+	ArrayList<String>actoresprotagónicos = new ArrayList<>();
+	ArrayList<String>directores = new ArrayList<>();
+	int duración;
+	ArrayList<String>categorias = new ArrayList<>();
+	int añoestreno;
+	int clasificacion;
+	int cantidaddevotos;
+	
+	public Pelicula(String titulo, String sinopsis, int duración, int añoestreno) {
+		this.titulo = titulo;
+		this.sinopsis = sinopsis;
+		this.duración = duración;
+		this.añoestreno = añoestreno;
+		this.clasificacion = 0;
+		this.cantidaddevotos = 0;
+	}
+	
+	
+	public double Getclasificacion() {
+		return clasificacion/cantidaddevotos;
+	}
+	
+	public void Añadircategoria(String c) {
+		this.categorias.add(c);
+	}
+	
+	public void Añadiractoresprotagonicos (String a) {
+		this.actoresprotagónicos.add(a);
+	}
+	
+	public void Añadirdirectores (String d) {
+		this.directores.add(d);
+	}
+	
+	 public int Getaño() {
+		 return this.añoestreno;
+	 }
+	 
+	 public String Gettitulo() {
+		 return this.titulo;
+	 }
+	 
+	 boolean Pertenececategoria(String cat) {
+		 return categorias.contains(cat);
+	 }
+	 
+	 public int Getcantdevotos() {
+		 return this.cantidaddevotos;
+	 }
+	 
+	 public void Setclasificacion(int clasif){
+		 this.clasificacion += clasif;
+		 this.cantidaddevotos += 1;
+	 }
+	 
+	 
 
 }
