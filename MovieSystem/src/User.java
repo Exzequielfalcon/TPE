eea6051e2bfc8f14999b2cc34e79cec588ae4509
exze <exzequielfalcon@gmail.com>
2018-11-05 11:26:08 -0300

    User modify

--- MovieSystem/src/User.java
+++ MovieSystem/src/User.java
@@ -6,9 +6,9 @@ import java.util.List;
 public abstract class User {
 	protected String nombre;
 	protected HashSet<String> generosfav = new HashSet();  //Generos favoritos
-	protected ArrayList<Pelicula> pelisvistas =new ArrayList(); //Peliculas Vistas
+	protected HashSet<Pelicula> pelisvistas =new HashSet(); //Peliculas Vistas
 	
-	public abstract List getGeneros();
+	public abstract Iterator getGeneros();
 	public abstract void ClasificarPeli(Pelicula p, int clasificacion);
 	public abstract void VerPelicula(Pelicula p);
 	public abstract Iterator getPelisVistas();
